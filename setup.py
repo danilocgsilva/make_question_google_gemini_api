9f1e076a2bd3d5a5bc1ca37fed8ee4d6849796c6
Danilo Silva <contact@danilocgsilva.me>
2025-11-24 20:48:12 -0300

    Removed test.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="make_question_google_gemini_api",
-    version="1.1.0",
+    version="1.1.1",
     packages=find_packages(),
     author="Danilo",
     author_email="contact@danilocgsilva.me",
--- test.py
+++
@@ -1,9 +0,0 @@
-from make_question_google_gemini_api.MakeQuestionGoogleGeminiApi import MakeQuestionGoogleGeminiApi
-from make_question_google_gemini_api.Config import Config
-import os
-
-mqgga = MakeQuestionGoogleGeminiApi()
-mqgga.make_question("Which is the Bangladesh capital?")
-answer = mqgga.get_results()
-# print(answer.to_json(formatted=True))
-print(answer)
